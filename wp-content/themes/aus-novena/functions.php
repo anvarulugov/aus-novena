de9a7acf9fb5f10f2af334facdb7c0ffd3848605
Anvar Ulugov <ulugov.anvar@gmail.com>
2021-12-01 04:59:55 +0500

    2-dars: Asset (css/js) fayllarni ulash

---
+++ wp-content/themes/aus-novena/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+define('AUS_NOVENA_URL', get_template_directory_uri());
+define('AUS_NOVENA_PATH', get_template_directory());
+
+
+function aus_novena_assets() {
+	wp_enqueue_style('bootstrap', AUS_NOVENA_URL . '/plugins/bootstrap/bootstrap.min.css', array(), '4.3.5', 'all');
+	wp_enqueue_style('icofont', AUS_NOVENA_URL . '/plugins/icofont/icofont.min.css', array(), '1.0.1', 'all');
+	wp_enqueue_style('slick', AUS_NOVENA_URL . '/plugins/slick-carousel/slick/slick.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('slick-theme', AUS_NOVENA_URL . '/plugins/slick-carousel/slick/slick-theme.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('style', AUS_NOVENA_URL . '/css/style.css', array(), '1.0.0', 'all');
+
+	wp_enqueue_script('jquery', AUS_NOVENA_URL . '/plugins/jquery/jquery.js', array(), '3.5.1', true);
+	wp_enqueue_script('bootstrap', AUS_NOVENA_URL . '/plugins/bootstrap/bootstrap.min.js', array(), '4.5.3', true);
+	wp_enqueue_script('slick', AUS_NOVENA_URL . '/plugins/slick-carousel/slick/slick.min.js', array(), '1.0.0', true);
+	wp_enqueue_script('shuffle', AUS_NOVENA_URL . '/plugins/shuffle/shuffle.min.js', array(), '1.0.0', true);
+
+	wp_enqueue_script('google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA', array(), '1.0.0', true);
+	wp_enqueue_script('gmap', AUS_NOVENA_URL . '/plugins/google-map/gmap.js', array(), '1.0.0', true);
+
+	wp_enqueue_script('script', AUS_NOVENA_URL . '/js/script.js', array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'aus_novena_assets');
---
+++ wp-content/themes/aus-novena/header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title>Novena- Health Care &amp; Medical template</title>
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Health Care Medical Html5 Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+	<meta name="author" content="Themefisher">
+	<meta name="generator" content="Themefisher Novena HTML Template v1.0">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body id="top">
+
+<header>
+	<div class="header-top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<ul class="top-bar-info list-inline-item pl-0 mb-0">
+						<li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>support@novena.com</a></li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Address Ta-134/A, New York, USA </li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+						<a href="tel:+23-345-67890">
+							<span>Call Now : </span>
+							<span class="h4">823-4565-13456</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+			<a class="navbar-brand" href="index.html">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="" class="img-fluid">
+			</a>
+
+			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain"
+			        aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="icofont-navigation-menu"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarmain">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+					<li class="nav-item"><a class="nav-link" href="service.html">Services</a></li>
+
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown"
+						   aria-haspopup="true" aria-expanded="false">Department <i class="icofont-thin-down"></i></a>
+						<ul class="dropdown-menu" aria-labelledby="dropdown02">
+							<li><a class="dropdown-item" href="department.html">Departments</a></li>
+							<li><a class="dropdown-item" href="department-single.html">Department Single</a></li>
+
+							<li class="dropdown dropdown-submenu dropright">
+								<a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
+
+								<ul class="dropdown-menu" aria-labelledby="dropdown0301">
+									<li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
+									<li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown"
+						   aria-haspopup="true" aria-expanded="false">Doctors <i class="icofont-thin-down"></i></a>
+						<ul class="dropdown-menu" aria-labelledby="dropdown03">
+							<li><a class="dropdown-item" href="doctor.html">Doctors</a></li>
+							<li><a class="dropdown-item" href="doctor-single.html">Doctor Single</a></li>
+							<li><a class="dropdown-item" href="appoinment.html">Appoinment</a></li>
+
+							<li class="dropdown dropdown-submenu dropleft">
+								<a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0501" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
+
+								<ul class="dropdown-menu" aria-labelledby="dropdown0501">
+									<li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
+									<li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown"
+						   aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
+						<ul class="dropdown-menu" aria-labelledby="dropdown05">
+							<li><a class="dropdown-item" href="blog-sidebar.html">Blog with Sidebar</a></li>
+							<li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
+						</ul>
+					</li>
+					<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
